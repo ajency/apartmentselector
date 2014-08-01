b2a18c599d2957cabb36cc82ef99657ce33ac8c7
Surekha Rane <surekha@ajency.in>
2014-08-01 10:19:06 +0530

    iframe files

---
+++ wp-content/themes/apartmentselector/apartments-iframe.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: apartments-iframe*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<iframe src="apartmentselector.skyi.com/apartment-selector/" width="100%">
+    <p>Your browser does not support iframes.</p>
+</iframe>
+
+</body>
+</html>
---
+++ wp-content/themes/apartmentselector/units-iframe.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: units-iframe*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<div>
+<iframe src="apartmentselector.skyi.com/apartment-selector/" width="320">
+    <p>Your browser does not support iframes.</p>
+</iframe>
+</div>
+</body>
+</html>
