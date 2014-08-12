147c0219a32626cdea6b7dc0d37b2f29fc183c22
Jeromie Fernandes <jeromie@ajency.in>
2014-08-12 15:09:37 +0530

    project css files + fonts updated

--- wp-content/themes/apartmentselector/spa/src/apps/header/header-view.js
+++ wp-content/themes/apartmentselector/spa/src/apps/header/header-view.js
@@ -11,7 +11,7 @@ define(['marionette'], function(Mariontte) {
       return HeaderView.__super__.constructor.apply(this, arguments);
     }
 
-    HeaderView.prototype.template = '<div class="backBtn {{textClass}}"> <a  class="text-white"><span class="glyphicon glyphicon-chevron-left "></span></a> </div> <div class="text-center"> <!--<h3 class="text-white m-t-15 ">{{textString}}</h3>--> <h3 class="text-white m-t-15 "><span class="slctnTxt">Your selection:</span> {{textString}} </h3> </div>';
+    HeaderView.prototype.template = '<div class="backBtn {{textClass}}"> <a  class="text-white"><span class="glyphicon glyphicon-chevron-left "></span></a> </div> <div class="text-center"> <h5 class="text-white m-t-20 bold text-uppercase"><span class="slctnTxt">Your selection:</span> {{textString}} </h3> </div>';
 
     HeaderView.prototype.className = "header navbar navbar-inverse";
 
