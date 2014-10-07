cbe40f3ec84207f582758e271a0201d09418464e
diana29 <diana@ajency.in>
2014-10-07 16:53:16 +0530

    towerid unit id hidden for email

--- wp-content/themes/apartmentselector/functions/formidable.php
+++ wp-content/themes/apartmentselector/functions/formidable.php
@@ -454,9 +454,13 @@ add_filter('frm_display_value','edit_frm_display_value',10,3);
 
 add_filter('frm_notification_attachment', 'add_my_attachment', 10, 3);
 function add_my_attachment($attachments, $form, $args){
-
-
-if($form->form_key == 'emailform' && $args['email_key']==1){ //change 5 to the ID of your form
+ 
+ 
+ 
+if($form->form_key == 'emailform' && $args['email_key']==1){ 
+	//$towerid = $args['entry']->metas['towerid'];
+	//$unitid  = $args['entry']->metas['unitid'];
+ 
  //add the file path here $attachments[] = str_replace("/","\\",ABSPATH) . 'wp-content\uploads\2014\07\2bhk_3d12.jpg'; //set the ABSOLUTE path to the image here
  
 }
